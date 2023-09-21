dbbb73f8a704ed710def4f504ced88574b61b4b9
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2023-09-21 17:13:51 +0200

    Update DashboardLayout.vue

--- front-end/src/layout/dashboard/DashboardLayout.vue
+++ front-end/src/layout/dashboard/DashboardLayout.vue
@@ -7,13 +7,7 @@
                 <sidebar-link to="/xsspayloads" name="XSS Payloads" icon="fas fa-file-code" />
                 <sidebar-link to="/settings" name="Settings" icon="fas fa-cogs" />
                 <li class="nav-item mt-5">
-                  <a target="_blank" href="https://twitter.com/IAmMandatory" class="nav-link">
-                    <i class="fab fa-twitter"></i>
-                    <p>By @IAmMandatory</p>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a target="_blank" href="https://github.com/mandatoryprogrammer/xsshunter-express" class="nav-link">
+                  <a target="_blank" href="https://github.com/l4rm4nd/xsshunter-express" class="nav-link">
                     <i class="fab fa-github"></i>
                     <p>Github Repo</p>
                   </a>
