09947705efd98849b59e016cf091c2d9449538f6
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2023-09-21 17:14:07 +0200

    Update ContentFooter.vue

--- front-end/src/layout/dashboard/ContentFooter.vue
+++ front-end/src/layout/dashboard/ContentFooter.vue
@@ -9,7 +9,7 @@
         </li>
       </ul>
       <div class="copyright">
-        © {{year}} by <a href="https://twitter.com/IAmMandatory" target="_blank"><code>@IAmMandatory</code></a>
+        © {{year}} by <a href="https://github.com/l4rm4nd" target="_blank"><code>LRVT</code></a>
       </div>
     </div>
   </footer>
